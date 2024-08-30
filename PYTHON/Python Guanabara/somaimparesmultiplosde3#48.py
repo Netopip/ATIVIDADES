a68044faeb8fd882256611afc74400df1ed2cd5b
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:22:26 -0300

    Create somaimparesmultiplosde3#48.py

---
+++ PYTHON/Python Guanabara/somaimparesmultiplosde3#48.py
@@ -0,0 +1,11 @@
+soma = 0
+cont = 0
+for n in range(1, 501, 2):
+    if n % 3 ==0:
+        soma += n    #soma = soma + n
+        cont += 1             #cont = cont + 1
+print(f'A soma de {cont} os valores é {soma}.')
+
+        
+      
+        
