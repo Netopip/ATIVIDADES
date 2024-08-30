b28f20161c51a250e7349bef443e4b7ba8bb4c95
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:33:21 -0300

    Delete Q4T1.py

--- atividades/s10/t1/Q4T1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    for i in range(10,1001,10):
-        if i == 1000:
-            print(f'{i}.')
-        else:
-            print(f'{i}', end=', ')
-
-if __name__ == '__main__':
-    main()
