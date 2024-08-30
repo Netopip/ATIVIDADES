8be38b0b13d5d6e3380452011911519abab25272
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:33:24 -0300

    Delete Q5T1.py

--- atividades/s10/t1/Q5T1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    maior = 0
-    for i in range(100):
-        numero = int(input())
-        if numero > maior:
-            maior = numero
-    print(maior)
-if __name__ == '__main__':
-    main()
