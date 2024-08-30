67814667f4a9917f14a95e1bb1d7127a1ac96f3f
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:39:34 -0300

    Create aula#13.py

---
+++ PYTHON/Python Guanabara/aula#13.py
@@ -0,0 +1,16 @@
+#print('oi')
+#print('oi')
+#print('oi')
+
+#for c in range(0, 6):
+ #   print(c)
+#print('fim')
+
+#for c in range(6, 0, -1):
+#    print(c)
+#print('FIM')
+
+#for c in range(0, 6):
+#    print('oi')
+#print('fim')
+
