e23a7333f7de39747caf34bd3719b8e5c8d5ffb7
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:33:37 -0300

    Delete Q4T2.py

--- atividades/s10/T2/Q4T2.py
+++
@@ -1,7 +0,0 @@
-def main():
-    for i in range(99,0,-11):
-        print(f'{i} bugs no software, pegue onze deles e conserte...')
-        print(f'Tecle "Ctrl+F5"')
-    print(f'Sem erros no software! Está estabilizado!')
-if __name__=='__main__':
-    main()
