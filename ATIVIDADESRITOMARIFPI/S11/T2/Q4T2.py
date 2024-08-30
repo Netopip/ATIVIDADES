dcf9748399cf6a72fe1e0ef9bfc88fc79a7af716
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:25:24 -0300

    Create Q4T2.py

---
+++ ATIVIDADESRITOMARIFPI/S11/T2/Q4T2.py
@@ -0,0 +1,31 @@
+def main():
+    total = 0
+    while True:
+        print('CÓDIGO  PRODUTO         PREÇO (R$)\n'
+              'H       Hamburger       5,50\n'
+              'C       Cheeseburger    6,80\n'
+              'M       Misto Quente    4,50\n'
+              'A       Americano       7,00\n'
+              'Q       Queijo Prato    4,00\n'
+              'X       PARA TOTAL DA CONTA')
+        opcao = str(input()).strip().upper()
+        if opcao == 'H':
+            total += 5.50
+        elif opcao == 'C' :
+            total += 6.80
+        elif opcao == 'M':
+            total += 4.50
+        elif opcao == 'A':
+            total += 7.00
+        elif opcao == 'Q':
+            total += 4.00
+        elif opcao == 'X':
+            break
+        else:
+            print('Opção inválida.')
+
+    print(f'{total:.2f}')
+if __name__ == '__main__':
+    main()
+
+
