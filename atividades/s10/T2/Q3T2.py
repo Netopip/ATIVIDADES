6d9fcf9f882a56194dfba6e80b999b5381a4e212
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:33:33 -0300

    Delete Q3T2.py

--- atividades/s10/T2/Q3T2.py
+++
@@ -1,7 +0,0 @@
-def main():
-    for i in range(99,251,7):
-        print(f'{i} bugs no software, pegue sete deles e conserte...')
-        print(f'Tecle "Ctrl+F5"')
-    print(f'Vamos fazer mais um café!')
-if __name__=='__main__':
-    main()
