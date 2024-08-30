156ff670b5abf2aa5704d13b7cb704120ae3fc37
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:20:36 -0300

    Create transformação.py

---
+++ PYTHON/Python Guanabara/transformação.py
@@ -0,0 +1,10 @@
+frase = 'Curso em Video Python'
+print(frase.replace('Python', 'Android')) #substitui de forma secundaria as palavras na string.
+print(frase.upper()) #Transformara as letras minusculas em maiusculas, e as maiusculas continuarao iguais.
+print(frase.lower()) #Transformara as letras maiusculas em minusculas, e as minusculas continuarao iguais.
+print(frase.capitalize())#transforma tudo em minusculo, so a primeira letra da frase ficara em maiuscula.
+print(frase.title())# transforam toda primeira letras das palavras em maiusculas.
+print(frase.strip())#remove espaços inuteis do começo e final de uma string.
+print(frase.split())#divide a string, apartir dos espaços.
+print('*'.join(frase))#junta as listas de uma string.#pode mudar o asterisco
+print(frase.upper().count('O'))
