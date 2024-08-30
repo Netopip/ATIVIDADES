dc2b99414a11b0186896545cbde3d830862df8a3
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:41:20 -0300

    Create carronovovelho.py

---
+++ PYTHON/Python Guanabara/carronovovelho.py
@@ -0,0 +1,8 @@
+tempo = int(input('Quantos anos tem seu carro:'))
+#if tempo <= 5:
+    #print('Carro novo')
+#else:
+    #print('Carro velho')
+print('carro novo' if tempo <= 5 else 'Carro velho')
+print('--FIM--')
+
