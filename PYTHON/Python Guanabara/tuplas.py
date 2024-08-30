6eb8027c93e2f696c9364dd562adf2a5d7eb90fa
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:20:27 -0300

    Create tuplas.py

---
+++ PYTHON/Python Guanabara/tuplas.py
@@ -0,0 +1,32 @@
+
+
+
+lanche = 'hamburguer', 'suco', 'pizza', 'pudim', 'batata frita'
+
+#for cont in range(0, len(lanche)):
+#   print(f' Eu vou comer {lanche[cont]} na posição {cont}')
+ 
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}')
+
+#print('Comi pra caramba!') 
+ 
+ 
+ 
+ 
+ 
+#print(lanche[1])
+#print(lanche[0])
+#print(lanche[3])
+#print(lanche[-1])
+#print(lanche[-2])
+#print(lanche[1:3])
+#print(lanche[2:])
+#print(lanche[:2])
+#print(lanche[-2:])
+#print(lanche[-3:])
+
+#lanche = 'hamburguer', 'suco', 'pizza', 'pudim'
+#lanche[1] = refrigerante
+#print(lanche[1])
+print(sorted(lanche))
