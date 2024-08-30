6aaa3a4ca97c16c2fd45c34317e19cf544ecff82
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:32:10 -0300

    Create exercicio#38.py

---
+++ PYTHON/Python Guanabara/exercicio#38.py
@@ -0,0 +1,9 @@
+numero_1 = float(input('1 numero:'))
+numero_2 = float(input('2 numero:'))
+
+if numero_1 > numero_2:
+    print('O primeiro numero é maior.')
+elif numero_1 < numero_2:
+    print('2 numero é maior')
+else:
+    print('Os numeros sao iguais')
