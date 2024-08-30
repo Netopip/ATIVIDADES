2fbf7af4b5fe33be33b4932a4a5fba83344b9e5a
Neto <fernandesneto1999@gmail.com>
2024-08-30 11:44:21 -0300

    Update Q5.py

--- ATIVIDADESRITOMARIFPI/S13/T2/Q5.py
+++ ATIVIDADESRITOMARIFPI/S13/T2/Q5.py
@@ -1,8 +1,21 @@
-
-
-
+def esta_ordenada(lista):
+    return lista == sorted(lista)#verifica se a lista digitada é igual a ela mesma na ordem crescente.
 
 def main():
     lista = []
-    while True:
-        
+    n = int(input())
+    for i in range(n):
+        numero = input()#a entrada será uma string
+        try:
+           numero = float(numero)#tentativa de transformar em numero de ponto flutuante(float)
+        except ValueError:
+            pass
+        lista.append(numero)#adicionando o numero a lista.
+
+    if esta_ordenada(lista): # verifica se a lista esta ordenada.
+        print('LISTA ORDENADA')
+    else:
+        print('LISTA NÃO ORDENADA')
+
+if __name__ == '__main__':
+    main()
