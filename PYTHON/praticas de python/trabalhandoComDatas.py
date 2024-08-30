a54b12396ea2bb185ce6d10aa24c1d4b8710a76d
Neto <fernandesneto1999@gmail.com>
2024-08-29 22:38:57 -0300

    Create trabalhandoComDatas.py

---
+++ PYTHON/praticas de python/trabalhandoComDatas.py
@@ -0,0 +1,14 @@
+import datetime
+
+data = datetime.date.today()
+print(data)
+
+#Para converter de string para data: strptime
+#para converter de data para string : srtftime
+# %Y para o ano com 4 digitos;
+# %m par ao mes;
+# %d para o dia;
+# %H para hora;
+# %M para minutos;
+# %S para segundos.s
+
