da7f37b040cd6d71c5bc70f6d6a110e4902ba32e
Neto <fernandesneto1999@gmail.com>
2024-09-04 11:11:27 -0300

    Update Q3.py

--- ATIVIDADESRITOMARIFPI/S14/T1/Q3.py
+++ ATIVIDADESRITOMARIFPI/S14/T1/Q3.py
@@ -1,6 +1,12 @@
 '''Dadas duas listas A e B contendo 20 elementos inteiros cada, gerar e exibir uma lista C do mesmo tamanho cujos elementos sejam a soma dos respectivos elementos de A e B.'''
 
 
+def criar_lista_c(lista_A, lista_B):
+    lista_C = []
+    for i in range(20):
+        lista_C.append(lista_A[i] + lista_B[i])
+    return lista_C
+
 def main():
     lista_A = []
     for i in range(20):
@@ -10,7 +16,9 @@ def main():
     for i in range(20):
         numero1= int(input())
         lista_B.append(numero1)
-    print(lista_A,lista_B)
+    lista_C = criar_lista_c(lista_A, lista_B)
+    print(f'{lista_A}\n{lista_B}')
+    print(lista_C)
 
 if __name__ == '__main__':
     main()
