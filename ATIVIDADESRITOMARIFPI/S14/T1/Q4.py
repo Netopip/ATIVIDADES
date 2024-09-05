ab591796fa3a0757228d2cffb150d263c054fa4d
Neto <fernandesneto1999@gmail.com>
2024-09-05 08:53:12 -0300

    Create Q4.py

---
+++ ATIVIDADESRITOMARIFPI/S14/T1/Q4.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    jogadores = []
+    alturas = []
+    for i in range(3):
+        nome = str(input().strip())
+        jogadores.append(nome)
+        altura = float(input().strip())
+        alturas.append(altura)
+    
