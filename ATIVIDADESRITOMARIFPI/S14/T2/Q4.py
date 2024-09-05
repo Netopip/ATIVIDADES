7b8ce87b4e734c13fe4a16edd81c2a2a8c4a19ca
Neto <fernandesneto1999@gmail.com>
2024-09-05 14:29:13 -0300

    Create Q4.py

---
+++ ATIVIDADESRITOMARIFPI/S14/T2/Q4.py
@@ -0,0 +1,39 @@
+'''Escreva um programa que ler dois conjuntos de números reais, armazenando-os em listas e calcule o produto escalar entre eles. Os conjuntos têm 5 elementos cada. Imprimir os dois conjuntos e o produto escalar, sendo que o produto escalar e dado por: (x1*y1 )+(x2*y2 )+(x3*y3 )+⋯+(xn*yn). Por exemplo, para as duas listas X e Y a seguir:
+
+        0       1       2       3       4
+X =     2       5       7       3       9
+
+Y =     3       8       1       0       4
+O produto escalar será: (2 x 3) + (5 x 8) + (7 x 1) + (3 x 0) + (9 x 4) = 89'''
+
+
+def produto_escalar(lista_x, lista_y):
+    nova_lista = []
+    for i in range(len(lista_x)):
+        multi = lista_x[i] * lista_y[i]
+        nova_lista.append(multi)
+    return nova_lista
+
+def soma_da_nova_lista(nova_lista):
+    soma = sum(nova_lista)
+    return soma
+
+def main():
+    lista_x = []
+    lista_y = []
+    for i in range(5):
+        numerox = float(input())
+        lista_x.append(int(numerox))
+    for i in range(5):
+        numeroy = float(input())
+        lista_y.append(int(numeroy))
+    
+    nova_lista = produto_escalar(lista_x, lista_y)
+    soma = soma_da_nova_lista(nova_lista)
+    print(f'{lista_x}\n{lista_y}')
+    print(soma)
+    
+if __name__ == '__main__':
+    main()
+        
+        
