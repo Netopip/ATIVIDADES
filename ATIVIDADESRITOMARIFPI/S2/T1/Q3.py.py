ae8f25c042cc1c0a63a24b3a496efee731469014
Neto <fernandesneto1999@gmail.com>
2024-09-09 20:44:57 -0300

    Create Q3.py.py

---
+++ ATIVIDADESRITOMARIFPI/S2/T1/Q3.py.py
@@ -0,0 +1,13 @@
+#entrada de dados
+n1 = input()
+n2 = input()
+
+#processamento de dados
+n1 = float(n1)
+n2 = float(n2)
+soma = n1 + n2
+multiplicacao = n1 * n2
+
+#saida de dados
+print(soma)
+print(multiplicacao)
