770aef82d554e23f569acc0c364ef4906938316a
Neto <fernandesneto1999@gmail.com>
2024-09-09 20:07:08 -0300

    Create Q3.py

---
+++ ATIVIDADESRITOMARIFPI/S1/T1/Q3.py
@@ -0,0 +1,7 @@
+'''Escreva um algoritmo/programa que calcule e exiba o número de segundos em um minuto, em uma hora e em um dia.'''
+
+def main():
+    print(f'60\n3600\n86400')
+    
+if __name__ == '__main__':
+    main()
