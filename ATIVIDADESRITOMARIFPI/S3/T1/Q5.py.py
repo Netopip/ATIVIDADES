5279b4860748af689cfce3a05231ab5396d0d6da
Neto <fernandesneto1999@gmail.com>
2024-09-09 20:45:33 -0300

    Create Q5.py.py

---
+++ ATIVIDADESRITOMARIFPI/S3/T1/Q5.py.py
@@ -0,0 +1,12 @@
+lua_estelar = int(input())
+essencia_dragao = int(input())
+lagrimas_fenix = int(input())
+
+preco_lua = 5
+preco_essencia = 3
+preco_lagrimas = 8
+
+custo_total = (lua_estelar * preco_lua) + (essencia_dragao * preco_essencia) + (lagrimas_fenix * preco_lagrimas)
+
+print(custo_total)
+
