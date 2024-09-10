e750c51eb56f1ae4f793904bb4e5305ce6dd4b50
Neto <fernandesneto1999@gmail.com>
2024-09-10 08:40:09 -0300

    Create Q5.py.py

---
+++ ATIVIDADESRITOMARIFPI/S4/T1/Q5.py.py
@@ -0,0 +1,13 @@
+altura = float(input())
+comprimento = float(input())
+largura = float (input())
+
+area = largura * comprimento 
+
+volume = largura * comprimento * altura
+
+paredes = 2 * altura * largura + 2 * altura * comprimento
+
+print(area)
+print(volume)
+print(paredes)
