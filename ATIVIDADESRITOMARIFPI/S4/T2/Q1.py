798c0bae38124a01c3452d96f53bd3c55221e215
Neto <fernandesneto1999@gmail.com>
2024-09-17 23:18:21 -0300

    Create Q1.py

---
+++ ATIVIDADESRITOMARIFPI/S4/T2/Q1.py
@@ -0,0 +1,19 @@
+valor1 = float(input())
+valor2 = float(input())
+
+soma = valor1 + valor2
+print(soma)
+concatenacao = str(float(valor1)) + str(int(valor2))
+print(concatenacao)
+multiplicacao = valor1 * valor2
+print(multiplicacao)
+multiplicacao_string = str(valor1) * int(valor2)
+print(multiplicacao_string)
+divisao = valor1 / valor2
+print(divisao)
+divisao_inteira = valor1 // valor2
+print(divisao_inteira)
+exponenciacao = valor1 ** valor2
+print(exponenciacao)
+modulo_resto = valor1 % valor2
+print(modulo_resto)
