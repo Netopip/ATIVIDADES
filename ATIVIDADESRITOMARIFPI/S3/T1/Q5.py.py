3e6effea6b5f50f57f168117bda2ac954b0694da
Neto <fernandesneto1999@gmail.com>
2024-09-17 23:18:17 -0300

    Update Q5.py.py

--- ATIVIDADESRITOMARIFPI/S3/T1/Q5.py.py
+++ ATIVIDADESRITOMARIFPI/S3/T1/Q5.py.py
@@ -5,8 +5,10 @@ lagrimas_fenix = int(input())
 preco_lua = 5
 preco_essencia = 3
 preco_lagrimas = 8
+if lua_estelar and essencia_dragao and lagrimas_fenix == 0:
+    print('')
+else:
+    custo_total = (lua_estelar * preco_lua) + (essencia_dragao * preco_essencia) + (lagrimas_fenix * preco_lagrimas)
+    print(custo_total)
 
-custo_total = (lua_estelar * preco_lua) + (essencia_dragao * preco_essencia) + (lagrimas_fenix * preco_lagrimas)
-
-print(custo_total)
 
