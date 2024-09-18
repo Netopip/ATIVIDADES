f7d64ed83f1a2eabca27408fb6f0c44c1e53f669
Neto <fernandesneto1999@gmail.com>
2024-09-17 23:19:22 -0300

    Create Q2.py

---
+++ ATIVIDADESRITOMARIFPI/S6/T2/Q2.py
@@ -0,0 +1,9 @@
+def main():
+    quantidade_dinheiro = float(input())
+
+    quantidade_arredondada = round(quantidade_dinheiro)
+
+    print(quantidade_arredondada )
+
+if __name__ == "__main__":
+    main()
