3009b24f60897dc564042b9817850be10e16024b
Neto <fernandesneto1999@gmail.com>
2024-09-20 18:26:20 -0300

    Create Q.py

---
+++ ATIVIDADESRITOMARIFPI/S15/T2/Q.py
@@ -0,0 +1,26 @@
+'''Faça um programa que leia e armazene em um array tridimensional contendo os valores do faturamento anual de uma empresa, especificados por filial e também mês a mês. Veja a estrutura do array na imagem anexa. Após a leitura dos dados faça o seguinte:
+
+Calcule o total de cada ano por filial;
+Calcule o total de todas as filiais por ano;
+Calcule o total do período para todas as filiais;
+Mostre todos os dados lidos e calculados de acordo com o período que ocorrer, por exemplo:
+    2014;Filial 1;Janeiro;210
+    ...
+    2014;Filial 1;Dezembro;463
+    TOTAL 2014 FILIAL 1;3526
+    2014;Filial 2;Janeiro;430
+    ...
+    2014;Filial 3;Dezembro;310
+    TOTAL 2014 FILIAL 3;3346
+    TOTAL 2014 TODAS FILIAIS;10727
+    2015;Filial 1;Janeiro;316
+    ...
+    2017;Filial 3;Dezembro;354
+    TOTAL 2017 FILIAL 3;3550
+    TOTAL 2017 TODAS FILIAIS;11123
+    TOTAL PERÍODO TODAS FILIAIS;42855
+
+
+
+Este exercício aceita os seguintes tipos de arquivos:'''
+
