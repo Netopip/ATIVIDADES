3cfe63332392c69109d938d53c5aeadc691c91e1
Neto <fernandesneto1999@gmail.com>
2024-10-23 16:28:33 -0300

    Create EXpoo.py

---
+++ AULAS POO/EXpoo.py
@@ -0,0 +1,13 @@
+class Carro:
+    def __init__(self,marca, modelo, cor , ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        
+    #def ligar(self):
+    
+carro1 = Carro('Fiat', 'Argo', 'Azul', 2024)
+print(f'O carro é um {carro1.marca}, {carro1.modelo} de cor {carro1.cor} do ano de {carro1.ano}.')
+        
+            
